47c6eaef3ba95ca74177509675ca2351ced9b3f1
kungfooman <lama12345@gmail.com>
2024-03-02 17:17:04 +0100

    Make WebREDUCE namespace available in `window`, add strict type checking via `jsconfig.json`

---
+++ web-reduce/src/index.js
@@ -0,0 +1,6 @@
+export * from './FileMenu.js';
+export * from './InputEditor.js';
+export * from './LoadPackagesDialogue.js';
+export * from './Main.js';
+export * from './TempFuncs.js';
+export * from './Templates.js';
