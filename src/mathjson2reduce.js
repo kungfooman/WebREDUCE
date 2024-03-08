68acd6ec373215d5b5ced23c9c319ee71d034327
kungfooman <lama12345@gmail.com>
2024-03-08 16:43:37 +0100

    src/mathjson2reduce.js: Implement more mathlive constructs

--- src/mathjson2reduce.js
+++ src/mathjson2reduce.js
@@ -7,7 +7,7 @@
  */
 function mathjson2reduce(mathjson) {
   const type = mathjson[0];
-  if (typeof mathjson === 'string') {
+  if (typeof mathjson === 'string' || typeof mathjson === 'number') {
     // take it as it is
     return mathjson;
   }
@@ -18,7 +18,32 @@ function mathjson2reduce(mathjson) {
     console.assert(triple === 'Triple');
     const bodyReduce = mathjson2reduce(body);
     return `for ${name} := ${from} : ${to} sum ${bodyReduce}`;
+  } else if (type === 'Multiply') {
+    const lhs = mathjson2reduce(mathjson[1]);
+    const rhs = mathjson2reduce(mathjson[2]);
+    return `(${lhs} * ${rhs})`;
+  } else if (type === 'Power') {
+    const lhs = mathjson2reduce(mathjson[1]);
+    const rhs = mathjson2reduce(mathjson[2]);
+    return `(${lhs} ** ${rhs})`;
+  } else if (type === 'Add') {
+    const lhs = mathjson2reduce(mathjson[1]);
+    const rhs = mathjson2reduce(mathjson[2]);
+    return `(${lhs} + ${rhs})`;
+  } else if (type === 'Subtract') {
+    const lhs = mathjson2reduce(mathjson[1]);
+    const rhs = mathjson2reduce(mathjson[2]);
+    return `(${lhs} + ${rhs})`;
+  } else if (type === 'Delimiter') {
+    const val = mathjson2reduce(mathjson[1]);
+    return `(${val})`;
   }
-  console.error("mathjson2reduce: unhandled element", mathjson);
+  console.assert(mathjson instanceof Array, "mathjson must be an array now");
+  // Convert to regular function call
+  // For example: ['Square', 'n']
+  // Turns into: Square(n)
+  const [,...args] = mathjson;
+  const argsReduce = args.map(mathjson2reduce).join(', ');
+  return `${type}(${argsReduce})`;
 }
 export {mathjson2reduce};
