1f90539356d20483262d4fb155e71b8036368b56
kungfooman <lama12345@gmail.com>
2024-03-14 17:24:03 +0100

    export saveInputs and add little example

--- src/saveInputs.js
+++ src/saveInputs.js
@@ -1,4 +1,9 @@
+/**
+ * @example
+ * console.log(saveInputs());
+ */
 function saveInputs() {
   const inputs = [...document.querySelectorAll(".InputDiv")].map(_ => _.innerText);
   return JSON.stringify(inputs, null, 2);
 }
+export {saveInputs};
